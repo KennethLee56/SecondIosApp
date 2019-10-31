6964e1aee4bf4135cb97b6fb100477317edd5cca
Kenneth Lee <kennethvlee@gmail.com>
2019-10-30 21:27:47 -0400

    Initial Commit

---
+++ SecondIosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondIosApp
+//
+//  Created by Lee, Kenneth Van on 10/30/19.
+//  Copyright © 2019 Lee, Kenneth Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
