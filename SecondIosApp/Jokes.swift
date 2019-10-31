b43bdd20afca399f097c0e6dc569183765984b26
Kenneth Lee <kennethvlee@gmail.com>
2019-10-30 22:04:21 -0400

    Added Joke logic.

---
+++ SecondIosApp/Jokes.swift
@@ -0,0 +1,24 @@
+//
+//  Jokes.swift
+//  SecondIosApp
+//
+//  Created by Lee, Kenneth Van on 10/30/19.
+//  Copyright © 2019 Lee, Kenneth Van. All rights reserved.
+//
+
+import Foundation
+
+class Jokes{
+    
+    // created a property that is an array of Strings
+    var jokes = [String]()
+    
+    // created an init method
+    // it's the equivalent of a Java constructor
+    init() {
+        // includes two Strings in the jokes array
+        jokes.append("What do you call a bear with no teeth! A gummy beer!")
+        jokes.append("If con is the opposite of progress, it must mean Congress is the opposite of Progress!")
+    }
+    
+}
